0f952d9918f135fae16bca738ae6a4115f4f0811
hjs1212 <38662922+hjs1212@users.noreply.github.com>
2021-05-03 00:19:36 +0900

    2020-05-02 Front update

--- WAS/repo/conf/urls.py
+++ WAS/repo/conf/urls.py
@@ -6,7 +6,7 @@ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+Class-based views 
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('xss/', views.xss, name='xss'),
+    path('xxe/', views.xxe, name='xxe'),
+    path('brokenAccess/', views.brokenAccess, name='brokenAccess'),
+    path('brokenAuth/', views.brokenAuth, name='brokenAuth'),
+    path('injection/', views.injection, name='injection'),
+    path('about/', views.about, name='about')
 ]
---
+++ WAS/repo/polls/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404, HttpResponseNotFound
+# Create your views here.
+
+def error(req) :
+    return Http404("Not Found")
+
+def index(req) :
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def home(req) :
+    return HttpResponse("Hello, world. You're at the polls home.")
+
+def xss(req) :
+    return HttpResponse("Hello, world. You're at the polls xss.")
+
+def xxe(req) :
+    return HttpResponse("Hello, world. You're at the polls xxe.")
+
+def brokenAccess(req) :
+    return HttpResponse("Hello, world. You're at the polls brokenAccess.")
+
+def brokenAuth(req) :
+    return HttpResponse("Hello, world. You're at the polls brokenAuth.")
+
+def injection(req) :
+    return HttpResponse("Hello, world. You're at the polls injection.")
+
+def about(req) :
+    return HttpResponse("Hello, world. You're at the polls about.")
