d4eae4e2661e66122f93b20018af7c243a3f4757
남민정 <audtjddbfl@naver.com>
2021-06-12 18:40:59 +0900

    test1

--- AccessControl/adminpage_AccessControl.py
+++ AccessControl/adminpage_AccessControl.py
@@ -10,10 +10,10 @@ import requests
 class DBHandler:
     def __init__(self):
         #Local Test
-        #host = "localhost"
-        #port = "27017"
-        host = "127.0.0.1"
-        port = "29528"
+        host = "localhost"
+        port = "27017"
+        #host = "127.0.0.1"
+        #port = "29528"
         self.client = MongoClient(host, int(port))
 
     def insert_item_one(self, data, db_name=None, collection_name=None):
@@ -90,51 +90,64 @@ chrome_driver_path = "E:\WASProjects\chromedriver_win32\chromedriver_win32\chrom
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
 
+def adminpage_login(url, user) :
+    browser = webdriver.Chrome('chromedriver.exe', options=options)
+    browser.get(url)
+
+    #로그인 정보 입력할 칸 찾기
+    input_id = browser.find_element_by_css_selector("#id_username")
+    input_pw = browser.find_element_by_css_selector("#id_password")
+
+    #로그인 정보 값 입력
+    input_id.send_keys(user['login'])
+    input_pw.send_keys(user['password'])
+
+    #로그인 클릭
+    browser.find_element_by_css_selector("#login-form > div.submit-row > input[type=submit]").click()
+    print(browser.current_url)
+
+    #관리자 페이지 로그인 후, 접근 가능한 페이지 찾기
+    html = browser.page_source
+    soup = BeautifulSoup(html,'html.parser')
+    link = soup.select('a')
+
+    #출력 관리자 페이지 로그인 후 접근 가능한 페이지 목록
+    af = open('AccessPage.txt', 'w')
+    sys.stdout = af
+
+    for n in link:
+        print(n.get('href'))
+
+    sys.stdout = sys.__stdout__
+    af.close()
+
+    #dictionary
+    with open("AccessPage.txt", "r") as f:
+        for line in f.readlines():
+            requestPart(line.strip())
+
+
+
 #login할 admin 페이지 경로
-login_url = 'https://soy3on.pythonanywhere.com/admin/login/?next=/admin/'
+#login_url = (input("login_url : "))
+#login_url = 'https://soy3on.pythonanywhere.com/admin/login/?next=/admin/'
 
 #관리자 계정 정보
-login_id = "dream"
-login_pw = "nana0813"
+#login_id = (input("login_id : "))
+#login_pw = (input("login_pw : "))
+#login_id = "dream"
+#login_pw = "nana0813"
 
-browser = webdriver.Chrome(chrome_driver_path,options=options)
-browser.get(login_url)
-
-#로그인 정보 입력할 칸 찾기
-input_id = browser.find_element_by_css_selector("#id_username")
-input_pw = browser.find_element_by_css_selector("#id_password")
-
-#로그인 정보 값 입력
-input_id.send_keys(login_id)
-input_pw.send_keys(login_pw)
-
-#로그인 클릭
-browser.find_element_by_css_selector("#login-form > div.submit-row > input[type=submit]").click()
-print(browser.current_url)
-
-#관리자 페이지 로그인 후, 접근 가능한 페이지 찾기
-html = browser.page_source
-soup = BeautifulSoup(html,'html.parser')
-link = soup.select('a')
-
-#출력 관리자 페이지 로그인 후 접근 가능한 페이지 목록
-af = open('AccessPage.txt', 'w')
-sys.stdout = af
-
-for n in link:
-    print(n.get('href'))
-
-sys.stdout = sys.__stdout__
-af.close()
-
-#dictionary
-with open("AccessPage.txt", "r") as f:
-    for line in f.readlines():
-        requestPart(line.strip())
+url, id, passwd = input('사용자 입력 값 : ').split()
+user = {
+    'login': id,
+    'password': passwd
+}
+adminpage_login(url,user)
 
 mongo.insert_item_one({"vulname":vulname,
                        "Type":type,
-                       "adminpage_Destination Page":login_url,
+                       "adminpage_Destination Page":url,
                        "adminpage_info":info,
                        "adminpage_Success":success,
                        "adminpage_Fail":fail},
